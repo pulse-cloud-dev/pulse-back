e6bf9d2ca515bbc181a4a73665d438fbc5d9c5a8
ssddo <ujm0524@naver.com>
2024-08-28 21:27:19 +0900

    [feature] memberRole enum 추가

--- src/main/java/pulse/back/common/enums/MemberRole.java
+++ src/main/java/pulse/back/common/enums/MemberRole.java
@@ -1,4 +1,8 @@
 package pulse.back.common.enums;
 
 public enum MemberRole {
+    SUPER_ADMIN,
+    ADMIN,
+    MANAGER,
+    USER;
 }
--- src/main/java/pulse/back/domain/member/service/MemberValidationService.java
+++ src/main/java/pulse/back/domain/member/service/MemberValidationService.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pulse.back.domain.member.dto.MemberLoginRequestDto;
+import pulse.back.entity.member.Member;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberValidationService {
-    public Member validateToLogin(MemberLoginRequestDto requestDto) {
+    public Member validateToLogin(MemberLoginRequestDto requestDto){
+        return null;
+    }
 }
