c22bf4014643a0f434b03fa9c84bc17507d57280
sando <72328552+imsando@users.noreply.github.com>
2025-01-14 17:52:32 +0900

    Update BackApplication.java

--- src/main/java/pulse/back/BackApplication.java
+++ src/main/java/pulse/back/BackApplication.java
@@ -2,11 +2,15 @@ package pulse.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("AWS_ACCESS_KEY", dotenv.get("AWS_ACCESS_KEY"));
+        System.setProperty("AWS_SECRET_KEY", dotenv.get("AWS_SECRET_KEY"));
 		SpringApplication.run(BackApplication.class, args);
 	}
 
