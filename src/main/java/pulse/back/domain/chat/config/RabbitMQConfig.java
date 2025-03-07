ddaafe14ad5798af90dc4c8cbb79ad2150db7efc
neo <maiload77@gmail.com>
2025-03-08 04:16:51 +0900

    [feature][PULSE-159] RabbitMQ 연결을 위한 기본 코드 추가

---
+++ src/main/java/pulse/back/domain/chat/config/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package pulse.back.domain.chat.config;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String CHAT_EXCHANGE = "chat.exchange";
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(CHAT_EXCHANGE);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+
+}
