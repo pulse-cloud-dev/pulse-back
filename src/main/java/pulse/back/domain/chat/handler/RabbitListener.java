e2fb37cf564b51958d78a9cd43b14d96aa523875
neo <maiload77@gmail.com>
2025-03-11 20:47:31 +0900

    [feature][PULSE-159] RabbitMQ 연결 로직 구현

--- src/main/java/pulse/back/domain/chat/handler/RabbitListener.java
+++ src/main/java/pulse/back/domain/chat/handler/RabbitListener.java
@@ -7,9 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import pulse.back.common.enums.ErrorCodes;
 import pulse.back.common.exception.CustomException;
-import pulse.back.domain.chat.dto.Message;
+import pulse.back.domain.chat.dto.entity.MessageDto;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.rabbitmq.Receiver;
 
@@ -20,32 +19,33 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 @RequiredArgsConstructor
 public class RabbitListener {
-    private final ConcurrentHashMap<String, Sinks.Many<Message>> roomSinkMap = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Sinks.Many<MessageDto>> roomSinkMap = new ConcurrentHashMap<>();
     private final Receiver receiver;
     private final ObjectMapper objectMapper;
 
-    // 채팅방 생성할 때 최초 1회 호출하여 Sink와 Rabbitmq 연결
-    public Mono<Void> addListener(String roomId) {
+    // 방 만들때 최초 1회 호출
+    public Flux<MessageDto> addListener(String roomId) {
+        // 현재 채팅방에 active 된 사용자 확인 후 seenBy 업데이트
         return receiver.consumeAutoAck(roomId)
-                .doOnNext(delivery -> {
-                    log.info("delivery : {}", delivery);
-                    // TODO: 현재 채팅방에 active 된 사용자 확인 후 seenBy 업데이트
-                    roomSinkMap.get(roomId).tryEmitNext(deliveryToMessage(delivery));
-                })
-                .then();
+                .doOnNext(delivery -> log.info("delivery : {}", delivery))
+                .map(this::deliveryToMessage);
+    }
+
+    public void tryEmitNext(String roomId, MessageDto messageDto) {
+        roomSinkMap.get(roomId).tryEmitNext(messageDto);
     }
 
     public void addRoomSink(String roomId) {
         roomSinkMap.put(roomId, Sinks.many().multicast().onBackpressureBuffer());
     }
 
-    public Flux<Message> getRoomSinkAsFlux(String roomId) {
+    public Flux<MessageDto> getRoomSinkAsFlux(String roomId) {
         return roomSinkMap.get(roomId).asFlux();
     }
 
-    private Message deliveryToMessage(Delivery delivery) {
+    private MessageDto deliveryToMessage(Delivery delivery) {
         try {
-            return objectMapper.readValue(delivery.getBody(), Message.class);
+            return objectMapper.readValue(delivery.getBody(), MessageDto.class);
         } catch (IOException e) {
             throw new CustomException(ErrorCodes.INTERNAL_SERVER_ERROR);
         }
